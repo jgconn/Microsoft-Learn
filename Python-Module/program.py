aefef7d3e5bfd9dd3f0ef7d4b596412573fbf3d9
Justin Conn <justinconn98@gmail.com>
2023-06-30 10:31:23 +1200

    Python logic

--- Python-Module/program.py
+++ Python-Module/program.py
@@ -14,3 +14,24 @@ print("Todays date is:", date.today())
 
 name = input("Enter your name: ")
 print(name)
+
+a = 16
+b = 25
+c = 27
+if a > b:
+    if b > c:
+        print ("a is greater than b and b is greater than c")
+    else: 
+        print ("a is greater than b and less than c")
+elif a == b:
+    print ("a is equal to b")
+else:
+    print ("a is less than b")
+
+a = 23
+b = 34
+if a == 34 or b == 34:
+    print(a + b)
+
+if a == 34 and b == 34:
+    print (a + b)
