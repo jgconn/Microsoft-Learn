7a63359ad46cdb569ebf7aad8ac533b2c9eddcbd
Justin Conn <justinconn98@gmail.com>
2023-07-03 11:24:12 +1200

    List module

--- Python-Module/program.py
+++ Python-Module/program.py
@@ -12,8 +12,8 @@ shuttle_landed_on_the_moon = "Apollo 11"
 print(date.today())
 print("Todays date is:", date.today())
 
-name = input("Enter your name: ")
-print(name)
+# name = input("Enter your name: ")
+# print(name)
 
 a = 16
 b = 25
@@ -120,3 +120,47 @@ print(round_up)
 
 round_down = floor(12.5)
 print(round_down)
+
+# lists planets = ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"]
+planets = ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"]
+
+print("The first planet is", planets[0])
+print("The second planet is", planets[1])
+print("The third planet is", planets[2])
+
+planets[3] = "Red Planet"
+print("Mars is also known as", planets[3])
+
+number_of_planets = len(planets)
+print("There are", number_of_planets, "planets in the solar system.")
+
+# pop()
+planets.pop()  # Goodbye, Pluto
+number_of_planets = len(planets)
+print("No, there are definitely", number_of_planets, "planets in the solar system.")
+
+# reverse 
+print("The last planet is", planets[-1])
+print("The penultimate planet is", planets[-2])
+
+# find a value in list
+jupiter_index = planets.index("Jupiter")
+print("Jupiter is the", jupiter_index + 1, "planet from the sun")
+
+gravity_on_planets = [0.378, 0.907, 1, 0.377, 2.36, 0.916, 0.889, 1.12]
+bus_weight = 12650 # in kilograms, on Earth
+
+print("On Earth, a double-decker bus weighs", bus_weight, "kg")
+print("The lightest a bus would be in the solar system is", bus_weight * min(gravity_on_planets), "kg")
+print("The heaviest a bus would be in the solar system is", bus_weight * max(gravity_on_planets), "kg")
+
+# join lists
+amalthea_group = ["Metis", "Adrastea", "Amalthea", "Thebe"]
+galilean_moons = ["Io", "Europa", "Ganymede", "Callisto"]
+
+regular_satellite_moons = amalthea_group + galilean_moons
+print("The regular satellite moons of Jupiter are", regular_satellite_moons)
+
+regular_satellite_moons.sort()
+print("The regular satellite moons of Jupiter are", regular_satellite_moons)
+
